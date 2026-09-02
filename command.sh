dba20c69ddf2e72ba67189bf0d5d1ea72a45e940
coder_mohan <amohan24@yahoo.com>
2026-09-03 02:57:40 +0530

    added command.sh file

---
+++ command.sh
@@ -0,0 +1,12 @@
+﻿# REPO_URL https://github.com/Mohan-aggarwal/jbsm
+
+git rm --cached job_sim
+git add .
+git commit -m "added changes"
+gh repo create jbsm --public --source=. --remote=origin --push
+git checkout -b update_logo
+git add .
+git commit -m "updated logo and link"
+git push origin update_logo
+gh pr create --base main --head update_logo --title "Update logo" --body "Replaces logo and updates link to DirtMate page"
+gh pr merge update_logo --merge
